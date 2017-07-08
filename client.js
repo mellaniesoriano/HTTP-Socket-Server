bbfc68622e9931a9901351ad35be53f8392a351e
mellaniesoriano <msoriano808@gmail.com>
2017-07-08 11:53:24 -1000

    parsed header for later use

--- client.js
+++ client.js
@@ -5,28 +5,34 @@ const net = require('net');
 var host = process.argv[2];
 var port = 80;
 
-if (host.indexOf(':') > -1){
-  port = host.split(':')[1];
-  host = host.split(':')[0];
-}
-if (!host) {
-  console.log('Invalid host. Please use node client.js <URL>');
-}
+var responseHeader = [];
 
-const client = net.connect(port, host, () => {
+if (!host) {
+  console.log("Oops, that didn't work! Learn how to use node : $ node -h client.js");
+} else {
+
+  if (host.indexOf(':') > -1){
+    port = host.split(':')[1];
+    host = host.split(':')[0];
+  }
+
+  const client = net.connect(port, host, () => {
   console.log('client connected');
 });
 
-client.write(`GET / HTTP/1.1
+  client.write(`GET / HTTP/1.1
 Date: ${new Date().toUTCString()}
 Host: ${host}
 User-Agent: Melly Beans
 Connection: close\r\n\r\n`);
 
-client.on('data', (data) => {
+  client.on('data', (data) => {
+    var dataSplit = data.toString().split('<HTML>');
+    responseHeader.push(dataSplit[0]);
     process.stdout.write(data);
-});
+  });
 
-client.on('close', () => {
-  console.log("connection closed");
-});
+  client.on('close', () => {
+    console.log("connection closed");
+  });
+}
