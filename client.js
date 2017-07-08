d9affb1134d13447e29c4a2d1723750c9a4c925c
mellaniesoriano <msoriano808@gmail.com>
2017-07-07 21:35:45 -1000

    added conditional for invalid host

--- client.js
+++ client.js
@@ -9,6 +9,9 @@ if (host.indexOf(':') > -1){
   port = host.split(':')[1];
   host = host.split(':')[0];
 }
+if (!host) {
+  console.log('Invalid host. Please use node client.js <URL>');
+}
 
 const client = net.connect(port, host, () => {
   console.log('client connected');
