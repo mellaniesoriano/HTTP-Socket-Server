2e688212b86df4f7950824a8272ebc872f4a0ed2
mellaniesoriano <msoriano808@gmail.com>
2017-07-07 20:51:20 -1000

    cleaned up

--- server.js
+++ server.js
@@ -6,62 +6,53 @@ const fs = require('fs');
 const indexHTML = fs.readFileSync('./public/index.html');
 const hydrogenHTML = fs.readFileSync('./public/hydrogen.html');
 const heliumHTML = fs.readFileSync('./public/helium.html');
-const error404 = fs.readFileSync('./public/error404.html');
+const error404 = fs.readFileSync('./public/404.html');
 const cssStyles = fs.readFileSync('./public/css/styles.css');
 
 const server = net.createServer( (request) => {
   request.on('data', (data) => {
     const dataArr = data.toString().split('\n');
-    // console.log('check data arr..', dataArr);
     const requestLine = dataArr[0].split(' ');
-    // console.log('request line..',requestLine);
     const path = requestLine[1];
-    // console.log('request 1', requestLine[1]);
-
-
 
     switch (path) {
       case '/':
       request.write(writeHeader('200 OK', 'text/html', indexHTML));
-      // request.end();
+      request.end();
 
       break;
 
       case '/index.html':
       request.write(writeHeader('200 OK', 'text/html', indexHTML));
-      // request.end();
+      request.end();
       break;
 
       case '/hydrogen.html':
       request.write(writeHeader('200 OK', 'text/html', hydrogenHTML));
-      // request.end();
+      request.end();
       break;
 
       case '/helium.html':
       request.write(writeHeader('200 OK', 'text/html', heliumHTML));
-      // request.end();
+      request.end();
       break;
 
       case '/public/css/styles.css':
       request.write(writeHeader('200 OK', 'text/css', cssStyles));
-      // request.end();
+      request.end();
       break;
 
       default:
-      request.write(writeHeader('404', 'text/html', error404));
-      // request.end();
-
+      request.write(writeHeader('404 BAD_REQUEST', 'text/html', error404));
+      request.end();
     }
   });
-
-}); // end createServer
-
-console.log(writeHeader('404', 'text/html', error404));
+});
 
 function writeHeader(status, fileType, fileName) {
   return `HTTP/1.1 ${status}
-Server: Mellanies Super Awesome Server
-Date: ${new Date()};
+Server: Mellanie's Super Awesome Server
+Date: ${new Date().toUTCString()};
 Content-Type: ${fileType}; charset=utf-8
 Content-Length: ${fileName.length}
 Connection: keep-alive
@@ -69,8 +60,6 @@ Connection: keep-alive
 ${fileName}`;
 }
 
-
-// listening to client
 server.listen(8080, () => {
   console.log(`Server listening on port 8080`);
 });
